67acd1d3110cf9413644dd7fd197fcd8940435b3
black-lemon <black-lemon@kali.berlin>
2020-11-04 11:47:40 +0100

    Added mission section + added sliding effect

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
  });
 
 
+//change navbar style on scroll
 $(window).scroll(function(){
 
  let position = $(this).scrollTop();
@@ -18,10 +19,29 @@ $(window).scroll(function(){
  }
  else{
   $('.nav-menu').removeClass('custom-navbar');
-
  }
- 
-})
+ })
+
+
+ //sliding effect for the section mission
+ $(window).scroll(function() {
+
+  let position = $(this).scrollTop();
+  console.log(position);
+
+  if(position >= 650){
+   $('.camera-img').addClass('from-left');
+   $('.mission-text').addClass('from-right');
+  }
+
+  
+  else{
+   
+   $('.camer-img').removeClass('from-left');
+   $('.mission-text').removeClass('from-right');
+  }
+  
+ })
  
 });
 
