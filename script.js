f851ebfc7e59c140f8fe1adfb9d73c2ee539333a
black-lemon <black-lemon@kali.berlin>
2020-11-04 13:47:41 +0100

    Added gallary + tweaks to active item select  event

--- script.js
+++ script.js
@@ -33,16 +33,34 @@ $(window).scroll(function(){
    $('.camera-img').addClass('from-left');
    $('.mission-text').addClass('from-right');
   }
-
-  
   else{
-   
    $('.camer-img').removeClass('from-left');
    $('.mission-text').removeClass('from-right');
   }
-  
  })
+
+
+
+ //Gallery select event
+$('.gallery-list-item').click(function () {
+ let value = $(this).attr('data-filter');
+
+ if( value === 'All'){
+  $('.filter').show(300);
+ }
+ else{
+  $('.filter').not('.' + value).hide(300);
+  $('.filter').filter('.' + value).show(300);
+ }
  
+})
+ 
+//dynamic active item 
+$('.gallery-list-item').click(function(){
+ $(this).addClass('active-item').siblings().removeClass('active-item');
+});
+
+
 });
 
 
