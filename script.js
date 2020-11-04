e496ebd5d76010e2edda5fd7733d40c57cdf7e49
black-lemon <black-lemon@kali.berlin>
2020-11-04 11:16:00 +0100

    Done Navbar

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 
 
+$(document).ready(function(){
+
+//toggle hamburgare menu button class to apply x animation
+ $('.nav-button').click(function(){
+  $('.nav-button').toggleClass('change');
+ });
+
+
+$(window).scroll(function(){
+
+ let position = $(this).scrollTop();
+ console.log(position);
+
+ if(position >= 200){
+  $('.nav-menu').addClass('custom-navbar');
+ }
+ else{
+  $('.nav-menu').removeClass('custom-navbar');
+
+ }
+ 
+})
+ 
+});
+
+
+
 
 
 
