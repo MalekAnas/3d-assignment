8442680cb0a171264c2adb0df2d1ec335e4f7ae3
black-lemon <black-lemon@kali.berlin>
2020-11-05 19:49:39 +0100

    Done with newsfeed side bar

--- script.js
+++ script.js
@@ -113,23 +113,7 @@ $('.gallery-list-item').click(function(){
 
 
  
- $("#contact-form").validate({
-  invalidHandler: function(event, validator) {
-    // 'this' refers to the form
-    var errors = validator.numberOfInvalids();
-    if (errors) {
-      var message = errors == 1
-        ? 'You missed 1 field. It has been highlighted'
-        : 'You missed ' + errors + ' fields. They have been highlighted';
-      $("div.error span").html(message);
-      $("div.error").show();
-    } else {
-      $("div.error").hide();
-    }
-  }
-});
-
- 
+  
  
 
  
